12a752ece91798591082fdb4ef328e4706fc0396
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2024-11-28 22:54:53 +0530

    Create oddevenIn.java

---
+++ oddevenIn.java
@@ -0,0 +1,17 @@
+public class oddevenIn {
+    public static void main(String[] args) {
+        System.out.println("enter the number");
+        int a=5;
+        if(a%2==0){
+            System.out.println("number is even");
+        }
+        else{
+            System.out.println("number is odd");
+        }
+    
+
+    }
+    
+}
+
+
