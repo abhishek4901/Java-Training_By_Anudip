9e0d9acb33f6fd6ffe004d21b09e4ad2a04734f2
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2024-11-28 23:15:55 +0530

    Create findLarge.java

---
+++ findLarge.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class findLarge {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int a = sc.nextInt();
+        System.out.println("enter the Second number");
+        int b = sc.nextInt();
+        if (a > b) {
+
+            System.out.println("First is greater  number");
+
+        } else {
+            System.out.println("Second  is greater  number");
+
+        }
+    }
+}
