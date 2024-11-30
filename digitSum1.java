6984dff23a8c0d0f897f90b9df23465c61a121d6
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2024-12-01 01:50:46 +0530

    Create digitSum1.java

---
+++ digitSum1.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class digitSum1 {
+    
+    public static void main(String[] args) {
+        System.out.println("enter the number");
+        Scanner sc = new Scanner(System.in);
+        int a = sc .nextInt();
+        // programmer can be do anything
+    }
+}
