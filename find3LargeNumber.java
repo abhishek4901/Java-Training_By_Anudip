49d6f634d050d6b20c5a7d719ab6b3b1ec7e0d27
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2024-12-01 15:35:05 +0530

    find larger in 3 number
    find the lager number in 3

---
+++ find3LargeNumber.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class find3LargeNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int a = sc.nextInt();
+        System.out.println("enter the Second number");
+        int b = sc.nextInt();
+
+        System.out.println("enter the third number");
+        int c = sc.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println("First is greater  number");
+
+            } else {
+                System.out.println("Second  is greater  number");
+
+            }
+        } else {
+            if (b > c) {
+                System.out.println("Second  is greater  number");
+
+            } else {
+                System.out.println(" Third is greater  number");
+                
+            }if(a==b||a==c){
+                System.out.println(" the numbeer are same number please change the number");
+
+            }
+
+        }
+
+    }
+}
