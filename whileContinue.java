e51dc589d600aef111c7fe9097eddf8d6536d0d1
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2024-12-05 15:39:05 +0530

    whileContinue.java

---
+++ whileContinue.java
@@ -0,0 +1,20 @@
+public class whileContinue {
+   
+        public static void main(String[] args) {
+            int i=0;
+            while(i<=10){
+    
+                if(i==4){ 
+                    i=i+5;
+                    continue;
+    
+                }
+                System.out.println(i);
+                i++;
+            }
+    
+        }
+        
+    }
+    
+    
