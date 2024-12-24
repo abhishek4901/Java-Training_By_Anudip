542463ebe41e439ae5a46f826ccc6d964a3eed21
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2024-12-24 23:54:25 +0530

    Create stringBufferBuilderEx.java

---
+++ stringBufferBuilderEx.java
@@ -0,0 +1,8 @@
+public class stringBufferBuilderEx {
+    public static void main(String[] args) {
+        StringBuffer sb1 =  new StringBuffer("hello\n"); //declear string Buffer
+        System.out.println(sb1 .append ("null\n"));
+        StringBuilder sb2 =  new StringBuilder("hello\n"); //  declear string builder
+        System.out.println(sb2 .append ("null\n"));
+    }
+}
