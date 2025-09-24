8f3b3441e54a74f2b7f6fe5ce0ca59aa42a6b203
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-09-24 22:44:17 +0530

    Update array2d.java

--- array2d.java
+++ array2d.java
@@ -3,7 +3,7 @@ public class array2d {
         
         int arr[] []= {{1,5,2,3,4,5}, {1,5,2,3,4,5}};
         // int arrr[] = {1,5,2,3,4,5};// this other way
-for(int i =0 ;i<arr.length;i++){
+for(int i =0 ;i<arr.length;i++){ 
 for(int j =0 ;j<arr[i].length;j++){
     System.out.println( arr[i][j]); 
         
