55df20feb30d1945baee1d675b3e6d0347246028
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-09-25 22:26:19 +0530

    Update ConstrStudent.java

--- Practice/ConstrStudent.java
+++ Practice/ConstrStudent.java
@@ -4,7 +4,7 @@
     int roll_no; 
     String name;
     double percent;
-//constructor bnane liye class name phir class attribute
+//constructor bnane liye class name phir class attribute 
     public ConstrStudent ( int roll_no,String name,double percent){
   this.roll_no =roll_no;//this keyword ka use confustion ko dur krne liye krte 
   this.name= name; //this.class attribute and =funnction argument
